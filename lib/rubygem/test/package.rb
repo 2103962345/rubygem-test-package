1fa2722140f935b6b3944b076366c0b51525af15
capotej <jcapote@gmail.com>
2016-12-15 18:01:55 -0500

    initial

---
+++ lib/rubygem/test/package.rb
@@ -0,0 +1,9 @@
+require "rubygem/test/package/version"
+
+module Rubygem
+  module Test
+    module Package
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rubygem/test/package/version.rb
@@ -0,0 +1,7 @@
+module Rubygem
+  module Test
+    module Package
+      VERSION = "0.1.0"
+    end
+  end
+end
